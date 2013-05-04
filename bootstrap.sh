8295bb6072df772a67250eb5952513f019023414
Szeto Bo <szetobo@gmail.com>
2013-05-04 20:55:51 +0800

    support archlinux environment bootstrap
    - remove compulsory building of ruby version, as some OS (like archlinux)
      already have system package to install

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,35 +5,35 @@
 # to used it as vagrant provision shell script, invoke with parameter 'vagrant'
 #
 
-case `lsb_release -is` in
-  Ubuntu)
-    # update system
-    sed -i 's/us\.archive\.ubuntu\.com/ftp\.cuhk\.edu\.hk\/pub\/Linux/g' /etc/apt/sources.list
-    sed -i 's/security\.ubuntu\.com/ftp\.cuhk\.edu\.hk\/pub\/Linux/g' /etc/apt/sources.list
-    apt-get update
-    # apt-get -y upgrade
+if (uname -a | grep -i ubuntu > /dev/null); then
+  # update package list
+  sed -i 's/us\.archive\.ubuntu\.com/ftp\.cuhk\.edu\.hk\/pub\/Linux/g' /etc/apt/sources.list
+  sed -i 's/security\.ubuntu\.com/ftp\.cuhk\.edu\.hk\/pub\/Linux/g' /etc/apt/sources.list
+  apt-get update
+  # apt-get -y upgrade
 
-    # get latest git-core repository
-    apt-get -y install python-software-properties
-    add-apt-repository -y ppa:git-core/ppa || add-apt-repository ppa:git-core/ppa
-    apt-get update
+  # get latest git-core repository
+  apt-get -y install python-software-properties
+  add-apt-repository -y ppa:git-core/ppa || add-apt-repository ppa:git-core/ppa
+  apt-get update
 
-    # install required packages
-    apt-get -y install build-essential git-core tig wget curl htop \
-      rake exuberant-ctags vim-gtk ack-grep zsh htop
-    apt-get -y install bison openssl libssl-dev libxslt1.1 libxslt1-dev \
-      libxml2 libxml2-dev libffi-dev libyaml-dev libxslt-dev autoconf \
-      libc6-dev libreadline6-dev zlib1g zlib1g-dev \
-      ruby-dev libopenssl-ruby
+  # install required packages
+  apt-get -y install build-essential git-core tig wget curl htop \
+    rake exuberant-ctags vim-gtk ack-grep zsh
+  apt-get -y install bison openssl libssl-dev libxslt1.1 libxslt1-dev \
+    libxml2 libxml2-dev libffi-dev libyaml-dev libxslt-dev autoconf \
+    libc6-dev libreadline6-dev zlib1g zlib1g-dev \
+    ruby-dev libopenssl-ruby
 
-     ;;
+elif (uname -a | grep -i arch > /dev/null); then
+  # update package list
+  pacman -Sy
+  pacman --noconfirm --needed -S base-devel git tig wget curl htop \
+    ruby vim-nox ctags ack zsh the_silver_searcher
+  pacman --noconfirm --needed -S bison openssl libxslt libxml2 libyaml libffi \
+    autoconf readline zlib
 
-   ManjaroLinux|ArchBang|ArchLinux)
-     ;;
-
-   *)
-     ;;
-esac
+fi
 
 #
 # install chruby
@@ -54,20 +54,6 @@ if [ ! -x /usr/local/bin/ruby-build ]; then
   ./install.sh
 fi
 
-#
-# install rubies
-#
-mkdir -p /opt/rubies
-if [ ! -d /opt/rubies/2.0.0-p0 ]; then
-  ruby-build 2.0.0-p0 /opt/rubies/2.0.0-p0
-fi
-
-# build 1.9.3-p392 with performance patch
-VERSION="1.9.3-p392"
-if [ ! -d /opt/rubies/$VERSION ]; then
-  wget -O /tmp/$VERSION https://raw.github.com/gist/5256849/2-$VERSION-patched.sh
-  ruby-build /tmp/$VERSION /opt/rubies/$VERSION
-fi
 
 if [[ $1 == "vagrant" ]]; then
   su -l -c '[ ! -d ~/.dotfiles ] && git clone git://github.com/szetobo/dotfiles.git ~/.dotfiles; ~/.dotfiles/run.sh install' vagrant
