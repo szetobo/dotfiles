456a16522ed738c1532fb03722f95bd74d7ccf39
Szeto Bo <szetobo@gmail.com>
2018-01-20 11:06:55 +0800

    Update ec2-init.sh to support ubuntu 16.04

--- ec2-init.sh
+++ ec2-init.sh
@@ -7,7 +7,33 @@ sudo -i
 
 source /etc/os-release
 
-if [[ "$NAME" = "Ubuntu" && "$VERSION_ID" = "14.04" ]]; then
+if [[ "$NAME" = "Ubuntu" && "$VERSION_ID" = "16.04" ]]; then
+  apt update
+
+  if [[ "$LANG" != "zh_TW.utf8" ]]; then
+    # setup proper locale
+    locale-gen zh_TW.utf8 en_HK.utf8
+    update-locale LC_ALL=zh_TW.utf8 LANG=zh_TW.utf8
+    # export LC_ALL=zh_TW.UTF8
+    # export LANG=zh_TW.UTF8
+  fi
+
+  timedatectl set-timezone Asia/Taipei
+
+  # if [[ "`cat /etc/timezone`" != "Asia/Taipei" ]]; then
+  #   # set local timezone
+  #   echo 'Asia/Taipei' | tee /etc/timezone
+  #   dpkg-reconfigure --frontend noninteractive tzdata
+  # fi
+
+  # install common tools
+  apt -y install wget curl htop silversearcher-ag
+
+  # upgrade installed packages & remove obsoleted one
+  apt -y upgrade
+  apt -y autoremove
+
+elif [[ "$NAME" = "Ubuntu" && "$VERSION_ID" = "14.04" ]]; then
   apt-get update
 
   if [[ "$LANG" != "en_HK.utf8" ]]; then
