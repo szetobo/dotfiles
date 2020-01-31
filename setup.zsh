d2ceb716dc79fd4eda2dca329810a3abce8cd400
Szeto Bo <szetobo@gmail.com>
2020-02-01 00:48:34 +0800

    Bug fix on setup.zsh
    - stow all config files in the very first step, in order to prevent
      accidentlly create config file so that stow may fail
    
    - .zsh already contains hstr config, no need to set to default config

--- setup.zsh
+++ setup.zsh
@@ -1,5 +1,10 @@
 #!/usr/bin/env zsh
 
+cd ~/.dotfiles/stow
+for s in *; do stow -t ~ $s; done
+
+mkdir -p ~/.psql_history
+
 if [ ! -d ~/.fzf ]; then
   git clone https://github.com/junegunn/fzf.git ~/.fzf
   yes n | ~/.fzf/install
@@ -9,12 +14,7 @@ if [ ! `whence hstr` ]; then
   sudo add-apt-repository -y ppa:ultradvorka/ppa
   sudo apt update
   sudo apt install hstr
-  hstr --show-configuration >> ~/.zshrc
+  # hstr --show-configuration >> ~/.zshrc
 fi
-
-cd ~/.dotfiles/stow
-for s in *; do stow -t ~ $s; done
-
-mkdir -p ~/.psql_history
 
 vi +PlugInstall +qall
